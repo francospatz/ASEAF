62cdb66ae9ea0573ed1fabb4a89127f62c51e8f9
francospatz <francospatzdilorenzo@gmail.com>
2022-05-19 23:23:28 +0200

    first commit/ heroku

---
+++ client/src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./Form";
+import Login from "./Login";
+import { Route, Routes } from 'react-router-dom'
+
+const Main = () => {
+  return <Routes>
+      <Route element={<Form/>} path='/'/>
+      <Route element={<Login/>} path='/login'/>
+  </Routes>;
+};
+
+export default Main;
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    return res.status(404).send({ message: 'Route' + req.url + ' Not found.' });
+});
+
+app.listen(port, () => { console.log(`listening on port ${port}`) })
