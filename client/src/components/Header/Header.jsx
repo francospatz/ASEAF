fdb5c8a17494159b46f4943d822328cdb4ec6776
francospatz <francospatzdilorenzo@gmail.com>
2022-05-19 23:31:29 +0200

    added localhost proxy

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div>
+    <Link to="/"><p>Form</p></Link>
+    <Link to="/login"><p>Login</p></Link>
+  </div>;
 };
 
 export default Header;
---
+++ client/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware'); // only for local run
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
+};
