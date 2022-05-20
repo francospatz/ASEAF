7188f4b896bab5a95672dee497023dcb16bc8618
francospatz <francospatzdilorenzo@gmail.com>
2022-05-20 21:25:10 +0200

    working on actions

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      es2020: true,
+      node: true
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module'
+    },
+    rules: {
+      semi: [
+        2,
+        'always'
+      ]
+    }
+  };
