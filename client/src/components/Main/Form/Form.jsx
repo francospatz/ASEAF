f9bf761463913ddfe6da9aa86779c9c2772da3e9
francospatz <francospatzdilorenzo@gmail.com>
2022-05-20 13:46:30 +0200

    Testing Google Tag Manager

--- client/src/components/Main/Form/Form.jsx
+++ client/src/components/Main/Form/Form.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Form = () => {
-  return <div>Form</div>;
+  return <div>
+    <a href="https://www.youtube.com/watch?v=mCdA4bJAGGk">Clica aquí y verás</a>
+  </div>;
 };
 
 export default Form;
