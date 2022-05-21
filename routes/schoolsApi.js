e04694cd67ac98d253beccc71d96402dafceb3a8
francospatz <francospatzdilorenzo@gmail.com>
2022-05-21 16:20:31 +0200

    fixing small errors

--- routes/schoolsApi.js
+++ routes/schoolsApi.js
@@ -1,5 +1,5 @@
 const routes = require('express').Router();
-const productApi = require('../controllers/schoolsApi');
+const schoolsApi = require('../controllers/schoolsApi');
 const protectedRoutes = require('../middlewares/verifiedToken');
 
 // usar protectedRoutes para la API de escuelas, así comprobamos que el JWT está activo antes de traer la info
