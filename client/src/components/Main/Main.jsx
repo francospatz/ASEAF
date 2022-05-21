88a63b66b9a5ea70ebe9e8bc8cac8ef63f8519a6
patryfuentes85 <98750081+patryfuentes85@users.noreply.github.com>
2022-05-21 17:16:32 +0200

    add test chart

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Form from "./Form";
 import Login from "./Login";
+import BarChart from "./BarChart";
+import LineChart from "./LineChart";
 import { Route, Routes } from 'react-router-dom';
 
 const Main = () => {
+
   return <Routes>
       <Route element={<Form/>} path='/'/>
       <Route element={<Login/>} path='/login'/>
+      <Route element={<BarChart/>} path='/bar'/>
+      <Route element={<LineChart/>} path='/line'/>
   </Routes>;
+  
 };
 
 export default Main;
+
+
