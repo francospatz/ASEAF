5fc0ce5bbb4f30920af3f7af8e1c5ebdf3f3c1c5
albertorg93 <futberto1993@gmail.com>
2022-05-21 19:39:04 +0200

    fixing form styles

--- client/src/components/Main/Form/Form.jsx
+++ client/src/components/Main/Form/Form.jsx
@@ -9,7 +9,7 @@ const Form = () => {
   return (
     
      <form className="formulario">
-       {/* <div className="conjunto"> */}
+       <div className="conjunto">
       <h2 className="titulo">Participa en el Día del Pijama 2021 rellenando este formulario</h2>
       <h4 className="subtitle">DATOS DEL COLEGIO QUE SE INSCRIBE</h4>
       <input type="text" name="colegio" placeholder="Colegio" required />
@@ -20,14 +20,16 @@ const Form = () => {
       <input type="text" name="nombre" placeholder="Nombre" required />
       <input type="text" name="apellidos"  placeholder="Apellido" required />
       <input type="email" name="email" placeholder="Correo electronico" required />
-      <input type="number" name="telefono" placeholder="Teléfono" required /> <br />
+      <input type="number" name="telefono" placeholder="Teléfono" required />
+      <div className="final">
       <select name="Puesto" id="puesto">
           <option value="profesor">Profesor</option>
           <option value="director">Director</option>
         </select>
         
         <input type="submit" value="Enviar"/>
-        {/* </div> */}
+        </div>
+    </div>
     </form> 
   );
 };
