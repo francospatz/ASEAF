29cb55817c24c856ad5567f771e019e51ed27b53
francospatz <francospatzdilorenzo@gmail.com>
2022-05-24 01:33:34 +0200

    working on graphs

--- models/school.js
+++ models/school.js
@@ -2,67 +2,60 @@ const pool = require("../utils/dbpg")
 require('dotenv').config();
 
 const getSchools = async () => {
-    /* let school;
-    let result;
 
-    try {
-      school = await pool.connect();
-      const data = await school.query(`select count(*) from edicion2021;`);
-      result = data.rows;
-      //console.log(result,"esto es result");
-    } catch (err) {
-      console.log(err);
-      throw err;
-    } finally {
-      school.release;
-    }
-    return result; */
-    try {
-      const data = await pool.query(`select count(*) from edicion2021;`);
-      result = data.rows;
-      return result
-    } catch (err) {
-      console.log(err);
-    }
-    
+  try {
+    const data = await pool.query(`select count(*) from edicion2021;`);
+    result = data.rows;
+    //pool.release()
+    return result
+  } catch (err) {
+    //pool.release()
+    console.log(err);
+  }
 
+};
 
+// Cam
+const getSchoolsbyCam = async () => {
 
-  };
-
-  // Cam
-  const getSchoolsbyCam = async () => {
-    /* let school;
-    let result;
-
-    try {
-      school = await pool.connect();
-      const data = await school.query(`SELECT estado, COUNT(nombre) as COLEGIOS_INSCRITOS,(SELECT COUNT(*) FROM edicion2021) as TOTAL
+  try {
+    const client = await pool.connect();
+    const data = await client.query(`SELECT estado, COUNT(nombre) as COLEGIOS_INSCRITOS,(SELECT COUNT(*) FROM edicion2021) as TOTAL
+    from edicion2021
+    GROUP BY estado
+    ORDER BY COLEGIOS_INSCRITOS DESC;`);
+    const result = data.rows;
+    client.release();
+    //pool.end(() => console.log("Pool has ended"))
+    return result
+  } catch (err) {
+    console.log(err)
+  } 
+  
+  /* const client = await pool.connect();
+  const data = await client.query(`SELECT estado, COUNT(nombre) as COLEGIOS_INSCRITOS,(SELECT COUNT(*) FROM edicion2021) as TOTAL
+  from edicion2021
+  GROUP BY estado
+  ORDER BY COLEGIOS_INSCRITOS DESC;`);
+  const result = data.rows;
+  client.release()
+  return result */
+/* 
+  try {
+    const data = await pool.query(`SELECT estado, COUNT(nombre) as COLEGIOS_INSCRITOS,(SELECT COUNT(*) FROM edicion2021) as TOTAL
       from edicion2021
       GROUP BY estado
       ORDER BY COLEGIOS_INSCRITOS DESC;`);
-      result = data.rows;
-      //console.log(result,"esto es result");
-    } catch (err) {
-      console.log(err);
-      throw err;
-    } finally {
-      school.release;
-    }
-    return result; */
-    try {
-      const data = await pool.query(`SELECT estado, COUNT(nombre) as COLEGIOS_INSCRITOS,(SELECT COUNT(*) FROM edicion2021) as TOTAL
-      from edicion2021
-      GROUP BY estado
-      ORDER BY COLEGIOS_INSCRITOS DESC;`);
-      result = data.rows;
-      return result
-    } catch (err) {
-      console.log(err);
-    }
-    
+    result = data.rows;
+    //pool.release()
+    return result
+  } catch (err) {
+    //pool.release()
+    console.log(err);
+  }
+ */
 
-  };
+};
 
 
-  module.exports = { getSchools, getSchoolsbyCam }
+module.exports = { getSchools, getSchoolsbyCam }
