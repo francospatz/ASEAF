a2f59bc04532ded210350825e6206bda106acbc4
patryfuentes85 <98750081+patryfuentes85@users.noreply.github.com>
2022-05-24 19:12:41 +0200

    add progressbar styles

--- client/src/components/Main/DashBoard/DashBoard.jsx
+++ client/src/components/Main/DashBoard/DashBoard.jsx
@@ -50,24 +50,43 @@ const DashBoard = () => {
       <div className='progress_bar_class'>
       {/* {totalCam2021()} */}
       <p>Andalucía</p><Progressbar bgcolor="#EF6424" progress='35' />
+      <h5 className='p_desc_class'>29 de 409 colegios</h5>
       <p>Aragón</p><Progressbar bgcolor="#EF6424" progress='44' />
+      <h5 className='p_desc_class'>20 de 409 colegios</h5>
       <p>Principado de Asturias</p><Progressbar bgcolor="#EF6424" progress='50' />
+      <h5 className='p_desc_class'>31 de 409 colegios</h5>
       <p>Islas Baleares</p><Progressbar bgcolor="#EF6424" progress='67' />
+      <h5 className='p_desc_class'>29 de 409 colegios</h5>
       <p>Canarias</p><Progressbar bgcolor="#EF6424" progress='45' />
+      <h5 className='p_desc_class'>32 de 409 colegios</h5>
       <p>Cantabria</p><Progressbar bgcolor="#EF6424" progress='25' />
+      <h5 className='p_desc_class'>29 de 409 colegios</h5>
       <p>Castilla y León</p><Progressbar bgcolor="#EF6424" progress='45' />
+      <h5 className='p_desc_class'>18 de 409 colegios</h5>
       <p>Castilla-La Mancha</p><Progressbar bgcolor="#EF6424" progress='50' />
+      <h5 className='p_desc_class'>23 de 409 colegios</h5>
       <p>Cataluña</p><Progressbar bgcolor="#EF6424" progress='33' />
+      <h5 className='p_desc_class'>66 de 409 colegios</h5>
       <p>Comunidad Valenciana</p><Progressbar bgcolor="#EF6424" progress='45' />
+      <h5 className='p_desc_class'>100 de 409 colegios</h5>
       <p>Extremadura</p><Progressbar bgcolor="#EF6424" progress='25' />
+      <h5 className='p_desc_class'>21 de 409 colegios</h5>
       <p>Galicia</p><Progressbar bgcolor="#EF6424" progress='45' />
+      <h5 className='p_desc_class'>23 de 409 colegios</h5>
       <p>Comunidad de Madrid</p><Progressbar bgcolor="#EF6424" progress='50' />
+      <h5 className='p_desc_class'>45 de 409 colegios</h5>
       <p>Región de Murcia</p><Progressbar bgcolor="#EF6424" progress='33' />
+      <h5 className='p_desc_class'>8 de 409 colegios</h5>
       <p>Comunidad de Navarra</p><Progressbar bgcolor="#EF6424" progress='45' />
+      <h5 className='p_desc_class'>23 de 409 colegios</h5>
       <p>País Vasco</p><Progressbar bgcolor="#EF6424" progress='45' />
+      <h5 className='p_desc_class'>32 de 409 colegios</h5>
       <p>La Rioja</p><Progressbar bgcolor="#EF6424" progress='45' />
+      <h5 className='p_desc_class'>7 de 409 colegios</h5>
       <p>Ciudad Autónoma de Ceuta</p><Progressbar bgcolor="#EF6424" progress='45' />
+      <h5 className='p_desc_class'>15 de 409 colegios</h5>
       <p>Ciudad Autónoma de Melilla</p><Progressbar bgcolor="#EF6424" progress='45' />
+      <h5 className='p_desc_class'>40 de 409 colegios</h5>
 
       </div>
 
